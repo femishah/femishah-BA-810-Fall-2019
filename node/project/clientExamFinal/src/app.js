1b4bfd3c8115def0d8c5ec814bc175eb64a92960
femishah <56010068+femishah@users.noreply.github.com>
2019-12-09 19:09:48 -0600

    cimmit exam

--- node/project/clientExamFinal/src/app.js
+++ node/project/clientExamFinal/src/app.js
@@ -4,7 +4,7 @@ export class App {
   configureRouter(config, router) {
     this.router = router;
     // config.addPipelineStep('authorize', AuthorizeStep); 
-    config.title = 'Widgets Universe!';
+    config.title = 'Widgets Exam!';
     config.map([
       {
         route: ['', 'widgets'],
